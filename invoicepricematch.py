ca9209b4fac24c8923b86c458fad9c8560d83bd5
goczangabor24 <goczangabor24@gmail.com>
2026-03-13 11:53:56 +0100

    Update invoicepricematch.py

--- invoicepricematch.py
+++ invoicepricematch.py
@@ -1001,9 +1001,9 @@ if st.session_state.price_check_ready:
                 vendor_manager = "" if pd.isna(first_row.iloc[2]) else str(first_row.iloc[2]).strip()
 
                 st.markdown(
-                    f" Don't forget to select"
-                    f" Issue Type: **{issue_type}**"
-                    f" Vendor Manager: **{vendor_manager}**"
+                    f"# Don't forget to select\n"
+                    f"# Issue Type: **{issue_type}**\n"
+                    f"# Vendor Manager: **{vendor_manager}**\n"
                 )
 
                 st.markdown("### JIRA Ticket Autofill")
