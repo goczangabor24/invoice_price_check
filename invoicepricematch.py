571fb44799aafca6867b31c0307adfd42b086d59
goczangabor24 <goczangabor24@gmail.com>
2026-03-13 12:08:33 +0100

    Update invoicepricematch.py

--- invoicepricematch.py
+++ invoicepricematch.py
@@ -1000,12 +1000,34 @@ if st.session_state.price_check_ready:
 
                 vendor_manager = "" if pd.isna(first_row.iloc[2]) else str(first_row.iloc[2]).strip()
 
-                st.markdown("### JIRA Ticket Autofill")
+            if issue_type == "Matina":
+                st.markdown(
+                    f"""
+                    <div style="
+                        background-color:#fff3cd;
+                        border:1px solid #ffe69c;
+                        color:#664d03;
+                        padding:16px;
+                        border-radius:10px;
+                        font-size:22px;
+                        font-weight:600;
+                        margin-bottom:12px;
+                    ">
+                        Don't forget to seledt Issue Type: {issue_type}<br>
+                    </div>
+                    """,
+                    unsafe_allow_html=True
+                )
+            else:   
                 st.markdown(
                     f" Don't forget to select"
-                    f" Issue Type: **{issue_type}**"
-                    f" Vendor Manager: **{vendor_manager}**"
+                    f" Issue Type: **Zooplus**"
                 )
+                st.markdown(
+                    f" f" Vendor Manager: **{vendor_manager}**"
+                    )
+                st.markdown("### JIRA Ticket Autofill")
+                
                 edited_jira_df = st.data_editor(
                     jira_autofill_df,
                     use_container_width=True,
